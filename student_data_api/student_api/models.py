8654acd5ea0413bf14e372beba62d9f3787ff926
iamjas6 <suhailuddin2002@gmail.com>
2025-02-14 18:53:22 +0530

    Models Updated

--- student_data_api/student_api/models.py
+++ student_data_api/student_api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.PositiveIntegerField()
+    address = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
